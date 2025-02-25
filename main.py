db8a350d1d2c5a5deb6a276f8b28ec43d5717a8a
notSaqlain <iamnotsaqlain@gmail.com>
2025-02-25 22:30:11 +0100

    Refactor main function in main.py to encapsulate webhook data retrieval and printing logic

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from GitBot import GitBot as gitbot
 
-webhook = gitbot("data.json")
 
-print("Event Type:", webhook.get_event_type())
-print("User Name:", webhook.get_user_name())
-print("Project Name:", webhook.get_project_name())
-print("Issue Title:", webhook.get_issue_title())
-print("Issue State:", webhook.get_issue_state())
-print("Issue URL:", webhook.get_issue_url())
-print("Issue IID:", webhook.get_issue_iid())
+def main():
+    webhook = gitbot("data.json")
+
+    print("Event Type:", webhook.get_event_type())
+    print("User Name:", webhook.get_user_name())
+    print("Project Name:", webhook.get_project_name())
+    print("Issue Title:", webhook.get_issue_title())
+    print("Issue State:", webhook.get_issue_state())
+    print("Issue URL:", webhook.get_issue_url())
+    print("Issue IID:", webhook.get_issue_iid())
+
+
+
+if __name__ == "__main__":
+    main()
