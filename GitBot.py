c15e3e91bc8391fe163e0f441b6a2ac6f0a17f64
notSaqlain <iamnotsaqlain@gmail.com>
2025-02-25 22:28:21 +0100

    Update data handling in GitBot to return empty list on load failure and access latest data for event and user details

--- GitBot.py
+++ GitBot.py
@@ -10,28 +10,29 @@ class GitBot:
             with open(self.file_path, "r") as file:
                 return json.load(file)
         except (FileNotFoundError, json.JSONDecodeError):
-            return {}
+            return []
+
 
     def get_event_type(self):
-        return self.data.get("event_type", "Unknown")
+        return self.data[-1].get("event_type", "Unknown") if self.data else "Unknown"
 
     def get_user_name(self):
-        return self.data.get("user", {}).get("name", "Unknown")
+        return self.data[-1].get("user", {}).get("name", "Unknown") if self.data else "Unknown"
 
     def get_user_username(self):
-        return self.data.get("user", {}).get("username", "Unknown")
+        return self.data[-1].get("user", {}).get("username", "Unknown") if self.data else "Unknown"
 
     def get_project_name(self):
-        return self.data.get("project", {}).get("name", "Unknown")
+        return self.data[-1].get("project", {}).get("name", "Unknown") if self.data else "Unknown"
 
     def get_issue_title(self):
-        return self.data.get("object_attributes", {}).get("title", "Unknown")
+        return self.data[-1].get("object_attributes", {}).get("title", "Unknown") if self.data else "Unknown"
 
     def get_issue_state(self):
-        return self.data.get("object_attributes", {}).get("state", "Unknown")
+        return self.data[-1].get("object_attributes", {}).get("state", "Unknown") if self.data else "Unknown"
 
     def get_issue_url(self):
-        return self.data.get("object_attributes", {}).get("url", "Unknown")
+        return self.data[-1].get("object_attributes", {}).get("url", "Unknown") if self.data else "Unknown"
 
     def get_issue_iid(self):
-        return self.data.get("object_attributes", {}).get("iid", "Unknown")
+        return self.data[-1].get("object_attributes", {}).get("iid", "Unknown") if self.data else "Unknown"
