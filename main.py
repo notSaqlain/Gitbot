67f4bf16d2eb27c2cb427ef43157940db3bc4a11
notSaqlain <iamnotsaqlain@gmail.com>
2025-02-25 21:31:17 +0100

    Add main.py to instantiate GitBot and print webhook data

---
+++ main.py
@@ -0,0 +1,12 @@
+from GitBot import GitBot
+
+# Create an instance and read the data
+webhook = GitBot("webhook_data.json")
+
+# Print values
+print("Event Type:", webhook.get_event_type())
+print("User Name:", webhook.get_user_name())
+print("Project Name:", webhook.get_project_name())
+print("Issue Title:", webhook.get_issue_title())
+print("Issue State:", webhook.get_issue_state())
+print("Issue URL:", webhook.get_issue_url())
