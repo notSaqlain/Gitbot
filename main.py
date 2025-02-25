4b9b536489d05a3abc0847dbf68077ca0a0a546b
notSaqlain <iamnotsaqlain@gmail.com>
2025-02-25 21:32:28 +0100

    Remove commented-out print statement in main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from GitBot import GitBot
 
 webhook = GitBot("webhook_data.json")
 
-# Print values
 print("Event Type:", webhook.get_event_type())
 print("User Name:", webhook.get_user_name())
 print("Project Name:", webhook.get_project_name())
